f9629d45e17a26e035245cc1d136241daaf8cefb
Marc Altmann <marcaltmann@posteo.de>
2025-06-12 01:49:26 +0200

    Add test for external link

--- explorer/core/tests.py
+++ explorer/core/tests.py
@@ -4,41 +4,43 @@ from django.test import TestCase
 from django.urls import reverse
 
 
+class FooterTests(TestCase):
+    def test_external_links(self):
+        """Test external links"""
+        response = self.client.get(reverse("core:welcome"))
+
+        self.assertContains(response, "https://github.com/marcaltmann/explorer")
+
+
 class CorePagesTests(TestCase):
     def test_welcome_page(self):
-        response = self.client.get(reverse("core:welcome"), follow=True)
+        response = self.client.get(reverse("core:welcome"))
 
-        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(
             response, "<h1 class='hero__title'>Explorer</h1>", html=True
         )
 
     def test_privacy_page(self):
-        response = self.client.get(reverse("core:privacy"), follow=True)
+        response = self.client.get(reverse("core:privacy"))
 
-        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, "<h1>Privacy</h1>", html=True)
 
     def test_contact_page(self):
-        response = self.client.get(reverse("core:contact"), follow=True)
+        response = self.client.get(reverse("core:contact"))
 
-        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, "<h1>Contact</h1>", html=True)
 
     def test_accessibility_page(self):
-        response = self.client.get(reverse("core:accessibility"), follow=True)
+        response = self.client.get(reverse("core:accessibility"))
 
-        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, "<h1>Accessibility</h1>", html=True)
 
     def test_terms_page(self):
-        response = self.client.get(reverse("core:terms"), follow=True)
+        response = self.client.get(reverse("core:terms"))
 
-        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, "<h1>Terms of Use</h1>", html=True)
 
     def test_legal_notice_page(self):
-        response = self.client.get(reverse("core:legal_notice"), follow=True)
+        response = self.client.get(reverse("core:legal_notice"))
 
-        self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertContains(response, "<h1>Legal Notice</h1>", html=True)
