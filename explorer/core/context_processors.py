58f56ace78edaa6a9e9478107f1031d3ed2d506b
Marc Altmann <marcaltmann@posteo.de>
2025-06-12 01:44:43 +0200

    Add repository link to footer

--- explorer/core/context_processors.py
+++ explorer/core/context_processors.py
@@ -3,6 +3,7 @@ from django.conf import settings
 def site(request):
     my_dict = {
         'site_name': settings.EXPLORER_SITE_NAME,
+        'site_version': settings.EXPLORER_SITE_VERSION,
     }
 
     return my_dict
