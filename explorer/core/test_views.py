175671a40e8c21ba8cb4a2942cc50c94e3afe4fd
Marc Altmann <marcaltmann@posteo.de>
2025-07-11 17:03:12 +0200

    Do not use i18n path prefixes

--- explorer/core/test_views.py
+++ explorer/core/test_views.py
@@ -4,42 +4,42 @@ from .views import welcome, legal_notice, accessibility, privacy, contact, terms
 
 
 def test_welcome(rf):
-    request = rf.get("/de/")
+    request = rf.get("/")  # The exact URL does not really matter.
     response = welcome(request)
     assert response.status_code == 200
 
 
 def test_legal_notice(rf):
-    request = rf.get("/de/pages/legal_notice/")
+    request = rf.get("/legal_notice/")
     response = legal_notice(request)
     assert response.status_code == 200
 
 
 def test_accessibility(rf):
-    request = rf.get("/de/pages/accessibility/")
+    request = rf.get("/accessibility/")
     response = accessibility(request)
     assert response.status_code == 200
 
 
 def test_privacy(rf):
-    request = rf.get("/de/pages/privacy/")
+    request = rf.get("/privacy/")
     response = privacy(request)
     assert response.status_code == 200
 
 
 def test_contact(rf):
-    request = rf.get("/de/pages/contact/")
+    request = rf.get("/contact/")
     response = contact(request)
     assert response.status_code == 200
 
 
 def test_terms(rf):
-    request = rf.get("/de/pages/terms/")
+    request = rf.get("/terms/")
     response = terms(request)
     assert response.status_code == 200
 
 
 def test_footer(rf):
-    request = rf.get("/de/")
+    request = rf.get("/")
     response = welcome(request)
     assertContains(response, "https://github.com/marcaltmann/explorer")
--- explorer/urls.py
+++ explorer/urls.py
@@ -1,19 +1,16 @@
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path("collections/", include("explorer.media_collections.urls")),
     path("collections/", include("explorer.resources.urls")),
     path("", include("explorer.core.urls")),
-)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DJANGO_ENV == "development":
     urlpatterns.append(path("__debug__/", include("debug_toolbar.urls")))
