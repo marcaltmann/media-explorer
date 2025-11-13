b97bd197734d2b8dd20bdbe14b123e4ddc46095d
Marc Altmann <marcaltmann@posteo.de>
2025-11-13 18:10:05 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+from litestar import Litestar, get
+from litestar.contrib.jinja import JinjaTemplateEngine
+from litestar.response import Template
+from litestar.static_files import create_static_files_router
+from litestar.template.config import TemplateConfig
+
+
+ASSETS_DIR = Path("assets")
+
+
+def on_startup():
+    ASSETS_DIR.mkdir(exist_ok=True)
+
+
+@get("/")
+async def index() -> Template:
+    title = "Media Explorer"
+    return Template(template_name="welcome.html.jinja2", context={"title": title})
+
+
+@get("/books/{book_id:int}")
+async def get_book(book_id: int) -> dict[str, int]:
+    return {"book_id": book_id}
+
+
+app = Litestar(
+    route_handlers=[
+        index,
+        get_book,
+        create_static_files_router(path="/static", directories=["assets"]),
+    ],
+    template_config=TemplateConfig(
+        directory=Path("templates"),
+        engine=JinjaTemplateEngine,
+    ),
+    on_startup=[on_startup],
+)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from mediaexplorer-ls!")
+
+
+if __name__ == "__main__":
+    main()
