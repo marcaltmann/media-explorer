53326d75d73de229364bbc6c8a281864c81e5a2a
Marc Altmann <marcaltmann@posteo.de>
2025-11-14 22:38:51 +0100

    Add a bunch of silent movies

--- app.py
+++ app.py
@@ -8,28 +8,19 @@ from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.plugins.sqlalchemy import (
     AsyncSessionConfig,
     SQLAlchemyAsyncConfig,
-    SQLAlchemyPlugin,
-    base,
+    SQLAlchemyPlugin
 )
 from litestar.response import Template
 from litestar.static_files import create_static_files_router
 from litestar.template.config import TemplateConfig
 from sqlalchemy import ForeignKey, func, select
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
-from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 from controllers.page_controller import PageController
 
-
-# The SQLAlchemy base includes a declarative model for you to use in your models.
-class Resource(base.BigIntAuditBase):
-    __tablename__ = "resource"
-    name: Mapped[str]
-    media_type: Mapped[str]
-    url: Mapped[str]
-    poster_url: Mapped[str]
-    duration: Mapped[float]
+from models import Resource
+from seeds import get_resources
 
 
 session_config = AsyncSessionConfig(expire_on_commit=False)
@@ -46,29 +37,7 @@ async def on_startup(app: Litestar) -> None:
         statement = select(func.count()).select_from(Resource)
         count = await session.execute(statement)
         if not count.scalar():
-            session.add_all([
-                Resource(
-                    name="Again to the Front (1952)",
-                    media_type="video/webm",
-                    url="https://upload.wikimedia.org/wikipedia/commons/f/f3/Again_to_the_Front_%281952%29_%E2%80%93_Mandarin_Chinese_dub.webm",
-                    poster_url="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Again_to_the_Front_%281952%29_%E2%80%93_Mandarin_Chinese_dub.webm/639px--Again_to_the_Front_%281952%29_%E2%80%93_Mandarin_Chinese_dub.webm.jpg?20240730091952",
-                    duration=4603.144,
-                ),
-                Resource(
-                    name="Trump Welcomes Citizens",
-                    media_type="video/webm",
-                    url="https://upload.wikimedia.org/wikipedia/commons/transcoded/7/74/PRESIDENT_DONALD_J._TRUMP_WELCOMES_AMERICA%E2%80%99S_NEWEST_CITIZENS_%F0%9F%87%BA%F0%9F%87%B8%F0%9F%A6%85.webm/PRESIDENT_DONALD_J._TRUMP_WELCOMES_AMERICA%E2%80%99S_NEWEST_CITIZENS_%F0%9F%87%BA%F0%9F%87%B8%F0%9F%A6%85.webm.1080p.vp9.webm",
-                    poster_url="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/PRESIDENT_DONALD_J._TRUMP_WELCOMES_AMERICA%E2%80%99S_NEWEST_CITIZENS_%F0%9F%87%BA%F0%9F%87%B8%F0%9F%A6%85.webm/800px--PRESIDENT_DONALD_J._TRUMP_WELCOMES_AMERICA%E2%80%99S_NEWEST_CITIZENS_%F0%9F%87%BA%F0%9F%87%B8%F0%9F%A6%85.webm.jpg",
-                    duration=103.908,
-                ),
-                Resource(
-                    name="Chinese Cuisine of Fried Rice",
-                    media_type="video/webm",
-                    url="https://upload.wikimedia.org/wikipedia/commons/e/eb/Chinese_cuisine_of_Fried_Rice.webm",
-                    poster_url="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Chinese_cuisine_of_Fried_Rice.webm/640px--Chinese_cuisine_of_Fried_Rice.webm.jpg?20130912140344",
-                    duration=187.649,
-                ),
-            ])
+            session.add_all(get_resources())
             await session.commit()
 
 
---
+++ models.py
@@ -0,0 +1,12 @@
+from litestar.plugins.sqlalchemy import base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+# The SQLAlchemy base includes a declarative model for you to use in your models.
+class Resource(base.BigIntAuditBase):
+    __tablename__ = "resource"
+    name: Mapped[str]
+    media_type: Mapped[str]
+    url: Mapped[str]
+    poster_url: Mapped[str]
+    duration: Mapped[float]
