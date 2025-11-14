b702e3377ae70db6d87e99c78cfc874d775e9e9a
Marc Altmann <marcaltmann@posteo.de>
2025-11-14 18:43:01 +0100

    Start putting templates in subdirectories

--- controllers/page_controller.py
+++ controllers/page_controller.py
@@ -8,20 +8,20 @@ class PageController(Controller):
 
     @get("/contact", name="contact")
     async def contact(self) -> Template:
-        return Template(template_name="contact.html.jinja")
+        return Template(template_name="pages/contact.html.jinja")
 
     @get("/privacy", name="privacy")
     async def privacy(self) -> Template:
-        return Template(template_name="privacy.html.jinja")
+        return Template(template_name="pages/privacy.html.jinja")
 
     @get("/accessibility", name="accessibility")
     async def accessibility(self) -> Template:
-        return Template(template_name="accessibility.html.jinja")
+        return Template(template_name="pages/accessibility.html.jinja")
 
     @get("/terms-of-use", name="terms-of-use")
     async def terms_of_use(self) -> Template:
-        return Template(template_name="terms_of_use.html.jinja")
+        return Template(template_name="pages/terms_of_use.html.jinja")
 
     @get("/legal-notice", name="legal-notice")
     async def legal_notice(self) -> Template:
-        return Template(template_name="legal_notice.html.jinja")
+        return Template(template_name="pages/legal_notice.html.jinja")
