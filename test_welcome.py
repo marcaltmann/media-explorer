14a7166a4e766d4662503d95f2ab5df19e33df1f
Marc Altmann <marcaltmann@posteo.de>
2025-12-06 01:08:58 +0100

    First test, but without test runner

---
+++ test_welcome.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+from litestar.status_codes import HTTP_200_OK
+from litestar.testing import TestClient
+
+from app import app
+
+app.debug = True
+
+
+def test_welcome():
+    with TestClient(app=app) as client:
+        response = client.get("/")
+        assert response.status_code == HTTP_200_OK
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        h1 = soup.find('h1')
+        assert h1.get_text() == "Media Explorer"
+
+
+if __name__ == "__main__":
+    test_welcome()
